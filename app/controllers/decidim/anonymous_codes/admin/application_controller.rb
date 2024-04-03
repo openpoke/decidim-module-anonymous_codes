894f7527c4e87eb4713d3f1b3f9edd9e609718ef
elviabth <ejbenedith@gmail.com>
2024-04-03 13:34:32 +0200

    add admin template for view

---
+++ app/controllers/decidim/anonymous_codes/admin/application_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Decidim
+  module AnonymousCodes
+    module Admin
+      class ApplicationController < Decidim::Admin::ApplicationController
+      end
+    end
+  end
+end
