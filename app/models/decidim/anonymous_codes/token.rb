f40ce8b7ecfd6eab5dbf7344a490354a7fc1fce8
Ivan Vergés <ivan@pokecode.net>
2024-04-03 12:14:31 +0200

    safety before destroy

--- app/models/decidim/anonymous_codes/token.rb
+++ app/models/decidim/anonymous_codes/token.rb
@@ -5,6 +5,11 @@ module Decidim
     class Token < ApplicationRecord
       self.table_name = :decidim_anonymous_codes_tokens
 
+      # prevent destroying if token has been used
+      before_destroy do
+        throw(:abort) if usage_count.positive?
+      end
+
       belongs_to :group, class_name: "Decidim::AnonymousCodes::Group"
       belongs_to :resource, polymorphic: true, optional: true
     end
--- spec/models/token_spec.rb
+++ spec/models/token_spec.rb
@@ -12,6 +12,22 @@ module Decidim
       describe "associations" do
         it { expect(described_class.reflect_on_association(:group).macro).to eq(:belongs_to) }
       end
+
+      context "when created" do
+        let!(:token) { create(:anonymous_codes_token) }
+
+        it "can be destroyed" do
+          expect { token.destroy }.to change(Token, :count).by(-1)
+        end
+
+        context "and has been used" do
+          let!(:token) { create(:anonymous_codes_token, usage_count: 1) }
+
+          it "cannot be destroyed" do
+            expect { token.destroy }.not_to change(Token, :count)
+          end
+        end
+      end
     end
   end
 end
