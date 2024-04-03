dc3b94f59ab1485aa9de0c06ddc528fe5497ded7
Ivan Vergés <ivan@pokecode.net>
2024-04-03 15:48:01 +0200

    configuration & token generator (#10)

--- lib/decidim/anonymous_codes.rb
+++ lib/decidim/anonymous_codes.rb
@@ -6,5 +6,23 @@ module Decidim
   # This namespace holds the logic of the `decidim-anonymous_codes` module.
   module AnonymousCodes
     include ActiveSupport::Configurable
+
+    config_accessor :default_token_length do
+      ENV.fetch("ANONYMOUS_CODES_DEFAULT_TOKEN_LENGTH", 10).to_i
+    end
+
+    config_accessor :token_style do
+      ENV.fetch("ANONYMOUS_CODES_TOKEN_STYLE", "alphanumeric")
+    end
+
+    def self.token_generator(length = nil)
+      length ||= AnonymousCodes.default_token_length
+      case AnonymousCodes.token_style
+      when "numeric"
+        SecureRandom.random_number(10**length).to_s.rjust(length, "0")
+      else
+        SecureRandom.alphanumeric(length).upcase
+      end
+    end
   end
 end
