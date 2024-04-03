183332cfd728fc49451233a39ff61488b532f2f2
elviabth <ejbenedith@gmail.com>
2024-04-03 17:31:14 +0200

    add table to index

--- app/controllers/decidim/anonymous_codes/admin/code_groups_controller.rb
+++ app/controllers/decidim/anonymous_codes/admin/code_groups_controller.rb
@@ -4,7 +4,19 @@ module Decidim
   module AnonymousCodes
     module Admin
       class CodeGroupsController < ApplicationController
+        helper_method :groups
+
         def index; end
+
+        def new; end
+
+        def create; end
+
+        private
+
+        def groups
+          AnonymousCodes::Group.all
+        end
       end
     end
   end
