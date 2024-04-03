4dd27b80c8cb14eb7f678f340cf7611238db2c97
elviabth <ejbenedith@gmail.com>
2024-04-03 13:25:25 +0200

    add code_groups_controller

---
+++ app/controllers/decidim/anonymous_codes/admin/code_groups_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Decidim
+  module AnonymousCodes
+    module Admin
+      class CodeGroupsController < ApplicationController
+        def index; end
+      end
+    end
+  end
+end
