81950299f6fc547fc41f0fa2a6e3baf6223ab4ad
elviabth <ejbenedith@gmail.com>
2024-04-09 15:16:35 +0200

    save refactored command test

--- spec/commands/admin/create_code_group_spec.rb
+++ spec/commands/admin/create_code_group_spec.rb
@@ -5,20 +5,45 @@ require "spec_helper"
 module Decidim
   module AnonymousCodes
     module Admin
-      describe Group, type: :model do
+      describe CreateCodeGroup do
         let(:current_organization) { create(:organization) }
+        let(:current_user) { create(:user, :confirmed, :admin, organization: current_organization) }
+        let(:form_params) do
+          {
+            title: "Sample Title",
+            expires_at: 1.day.from_now,
+            active: true,
+            max_reuses: 10
+          }
+        end
+        let(:form) do
+          CodeGroupForm.from_params(
+            form_params
+          ).with_context(
+            current_organization: current_organization
+          )
+        end
+        let(:command) { described_class.new(form) }
 
-        describe "#create" do
-          it "creates a code group successfully" do
-            code_group = Decidim::AnonymousCodes::Group.create(
-              title: "Sample Code Group",
-              expires_at: 10.days.from_now,
-              active: true,
-              max_reuses: 10,
-              organization: current_organization
-            )
+        describe "when the form is invalid" do
+          before do
+            allow(form).to receive(:invalid?).and_return(true)
+          end
 
-            expect(code_group).to be_valid
+          it "broadcasts invalid" do
+            expect { command.call }.to broadcast(:invalid)
+          end
+
+          it "doesn't create questionnaire answers" do
+            expect do
+              command.call
+            end.not_to change(Group, :count)
+          end
+        end
+
+        describe "when the form is valid" do
+          it "broadcasts ok" do
+            expect { command.call }.to broadcast(:ok)
           end
         end
       end
