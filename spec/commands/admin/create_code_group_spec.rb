472559a2d099de3602f723ce80936e700d52ce1b
elviabth <ejbenedith@gmail.com>
2024-04-09 15:08:43 +0200

    refactor create_code_group_spec

--- spec/commands/admin/create_code_group_spec.rb
+++ spec/commands/admin/create_code_group_spec.rb
@@ -4,20 +4,22 @@ require "spec_helper"
 
 module Decidim
   module AnonymousCodes
-    describe Group, type: :model do
-      let(:current_organization) { create(:organization) }
+    module Admin
+      describe Group, type: :model do
+        let(:current_organization) { create(:organization) }
 
-      describe "#create" do
-        it "creates a code group successfully" do
-          code_group = Decidim::AnonymousCodes::Group.create(
-            title: "Sample Code Group",
-            expires_at: 10.days.from_now,
-            active: true,
-            max_reuses: 10,
-            organization: current_organization
-          )
+        describe "#create" do
+          it "creates a code group successfully" do
+            code_group = Decidim::AnonymousCodes::Group.create(
+              title: "Sample Code Group",
+              expires_at: 10.days.from_now,
+              active: true,
+              max_reuses: 10,
+              organization: current_organization
+            )
 
-          expect(code_group).to be_valid
+            expect(code_group).to be_valid
+          end
         end
       end
     end
