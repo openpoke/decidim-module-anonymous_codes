e810bbfa6e7145ae1bcdf9df3b970862e86e3b36
elviabth <ejbenedith@gmail.com>
2024-04-11 11:26:59 +0200

    add code_group_form_spec

---
+++ spec/forms/admin/code_group_form_spec.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim
+  module AnonymousCodes
+    module Admin
+      describe CodeGroupForm do
+        let(:current_organization) { create(:organization) }
+        let(:current_user) { create :user, organization: current_organization }
+        let(:form_params) do
+          {
+            title_en: "Sample Title",
+            title_ca: "Títol de la mostra",
+            title_es: "Titulo de ejemplo",
+            expires_at: 1.day.from_now,
+            active: true,
+            max_reuses: 10
+          }
+        end
+        let(:form) do
+          CodeGroupForm.from_params(
+            form_params
+          ).with_context(
+            current_organization: current_organization
+          )
+        end
+
+        describe "validations" do
+          context "when all attributes are valid" do
+            it "is valid" do
+              expect(form).to be_valid
+            end
+          end
+
+          context "when title is missing" do
+            let(:form_params) do
+              {
+                title_en: "Sample Title",
+                title_ca: "Títol de la mostra",
+                title_es: "Titulo de ejemplo",
+                expires_at: 1.day.from_now,
+                active: true,
+                max_reuses: 0
+              }
+            end
+            let(:form) { described_class.new(form_params) }
+
+            it "is invalid" do
+              expect(form).not_to be_valid
+              expect(form.errors[:max_reuses]).to include("must be greater than 0")
+            end
+          end
+        end
+      end
+    end
+  end
+end
