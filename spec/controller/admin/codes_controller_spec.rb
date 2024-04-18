0bf963d74d2df151f98c3f971e1bf391d034a66a
elviabth <ejbenedith@gmail.com>
2024-04-18 10:12:02 +0200

    fix codes_controller_spec get index check

--- spec/controller/admin/codes_controller_spec.rb
+++ spec/controller/admin/codes_controller_spec.rb
@@ -7,9 +7,11 @@ module Decidim
     module Admin
       describe CodesController, type: :controller do
         routes { Decidim::AnonymousCodes::AdminEngine.routes }
+
         let(:current_user) { create(:user, :confirmed, :admin, organization: current_organization) }
         let(:current_organization) { create(:organization) }
         let!(:group) { create(:anonymous_codes_group, expires_at: 1.day.from_now, active: true, max_reuses: 10, organization: current_organization) }
+        let(:code_group) { create(:anonymous_codes_group, organization: current_organization) }
 
         before do
           request.env["decidim.current_organization"] = current_organization
@@ -17,8 +19,8 @@ module Decidim
         end
 
         describe "GET #index" do
-          let!(:token1) { Token.create(group: group, created_at: 2.days.ago) }
-          let!(:token2) { Token.create(group: group, created_at: 1.day.ago) }
+          let!(:token1) { Token.create(group: code_group, created_at: 2.days.ago) }
+          let!(:token2) { Token.create(group: code_group, created_at: 1.day.ago) }
 
           it "enforces permission to view anonymous code tokens" do
             expect(controller).to receive(:enforce_permission_to).with(:view, :anonymous_code_token)
